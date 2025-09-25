728b0881822a18edc6440cff2933e79ddd7fde3a
Martin Hauke <martin.hauke@sandstorm.de>
2025-09-25 15:50:19 +0200

    add obsidian

---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,9 @@
+-- see: https://github.com/epwalsh/obsidian.nvim
+require('obsidian').setup {
+  workspaces = {
+    {
+      name = 'notes',
+      path = '/Users/martinhauke/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes',
+    },
+  },
+}
