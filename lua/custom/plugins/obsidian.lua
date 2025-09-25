fa487158d9cfff57190f4cd853c9161379c6097d
Martin Hauke <martin.hauke@sandstorm.de>
2025-09-25 22:21:55 +0200

    add and configur obsidian

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,9 +1,89 @@
 -- see: https://github.com/epwalsh/obsidian.nvim
-require('obsidian').setup {
-  workspaces = {
-    {
-      name = 'notes',
-      path = '/Users/martinhauke/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes',
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+
+    -- see below for full list of optional dependencies 👇
+  },
+  keys = {
+    { '<leader>on', ':ObsidianNew<CR>', desc = 'New [o]bsidian [n]ote' },
+    { '<leader>od', ':ObsidianToday<CR>', desc = 'Open/Create [o]bsidian [d]aily for today' },
+    { '<leader>soo', ':ObsidianSearch<CR>', desc = '[s]earch in [o]bsidian n[o]tes' },
+    { '<leader>sot', ':ObsidianTags<CR>', desc = '[s]earch in [o]bsidian [t]ags' },
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'notes',
+        path = '/Users/martinhauke/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes',
+      },
+    },
+    -- Keep notes in a specific subdirectory
+    notes_subdir = 'notes',
+    -- Where to put new notes. Valid options are
+    --  * "current_dir" - put new notes in same directory as the current buffer.
+    --  * "notes_subdir" - put new notes in the default notes subdirectory.
+    new_notes_location = 'notes_subdir',
+
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ''
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.date '%Y-%m-%d') .. '-' .. suffix
+    end,
+
+    templates = {
+      folder = 'templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M',
+      substitutions = {
+        date_with_day = function()
+          return tostring(os.date '%d.%m.%Y - %A')
+        end,
+      },
+    },
+
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = 'dailies',
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = '%Y-%m-%d-%A',
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = '%B %-d, %Y',
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { 'daily-notes' },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = 'daily.md',
+    },
+
+    ui = {
+      enable = false,
     },
   },
 }
