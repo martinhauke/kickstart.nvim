20c279e1b9ef97a7c0eb53411194f830122cdfeb
Martin Hauke <martin.hauke@sandstorm.de>
2025-09-28 13:39:58 +0200

    add inline markdown rendering

---
+++ lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,32 @@
+-- see: https://github.com/epwalsh/obsidian.nvim
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-mini/mini.nvim',
+  }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    checkbox = {
+      custom = {
+        moved = {
+          raw = '[>]',
+          rendered = '󰛂 ',
+        },
+        canceled = {
+          raw = '[~]',
+          rendered = '󰅗 ',
+          highlight = 'DiagnosticWarn',
+        },
+        happy = {
+          raw = '[+]',
+          rendered = ' ',
+          highlight = 'DiagnosticOk',
+        },
+      },
+    },
+  },
+}
