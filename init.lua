40682d4a7c839afb29b5694e5bd1b49c2b3c9d1e
Martin Hauke <martin.hauke@sandstorm.de>
2025-09-28 14:35:10 +0200

    update plugins

--- init.lua
+++ init.lua
@@ -880,7 +880,6 @@ require('lazy').setup({
       signature = { enabled = true },
     },
   },
-
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
